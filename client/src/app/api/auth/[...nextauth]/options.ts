4d47d1a206387310cc38699ba93544a2bec1280a
David Pietrocola <pietrocoladavid@gmail.com>
2024-03-05 22:27:00 -0500

    feat: adds adaptor

--- client/src/app/api/auth/[...nextauth]/options.ts
+++ client/src/app/api/auth/[...nextauth]/options.ts
@@ -1,8 +1,30 @@
 import type { NextAuthOptions } from 'next-auth'
-import { GithubProfile } from 'next-auth/providers/github';
+import Github, { GithubProfile } from 'next-auth/providers/github';
 import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
+import NextAuth from "next-auth"
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import { connectToDB } from '@/lib/database';
+import { MongoClient } from "mongodb"
+import type { Adapter } from 'next-auth/adapters';
+
+const client = new MongoClient(process.env.MONGODB_URL as string);
+const clientPromise = client.connect();
 
 export const options: NextAuthOptions = {
+  // TODO: This adapter "disables" the callback.
+  //https://github.com/nextauthjs/next-auth/issues/9493#issuecomment-1872580831
+  // adapter: MongoDBAdapter(
+  //   clientPromise,
+  //   {
+  //     collections: {
+  //       Users: "user",
+  //       Sessions: "sessions",
+  //       VerificationTokens: "verificationTokens",
+  //       Accounts: "accounts",
+  //     },
+  //     databaseName: "comp308_Auth",
+  //   }
+  // ) as Adapter,
   providers: [
     GoogleProvider({
       profile: (profile: GoogleProfile) => {
@@ -18,8 +40,5 @@ export const options: NextAuthOptions = {
     },
     )
   ],
-  pages:
-  {
-    signIn: '/auth/signin',
-  },
+
 }
